90ba62c51a8839925071306ddfbbc9a5b643badb
tenthirtyone <alex.sherbuck@magiceden.io>
2025-10-07 14:07:15 -0500

    packs admin

---
+++ src/packs/base/PacksAdmin.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import "./PacksStorage.sol";
+import {Errors} from "../../common/Errors.sol";
+
+/// @title PacksAdmin
+/// @notice Handles admin configuration functions for Packs contract
+/// @dev Abstract contract with admin helpers - storage accessed from PacksStorage
+abstract contract PacksAdmin is PacksStorage {
+    
+    // ---------- Cosigner Management ----------
+    
+    function _addCosigner(address cosigner_) internal {
+        if (cosigner_ == address(0)) revert Errors.CosignerAddressZero();
+        if (isCosigner[cosigner_]) revert Errors.AlreadyCosigner();
+        isCosigner[cosigner_] = true;
+        emit CosignerAdded(cosigner_);
+    }
+    
+    function _removeCosigner(address cosigner_) internal {
+        if (!isCosigner[cosigner_]) revert Errors.NotActiveCosigner();
+        isCosigner[cosigner_] = false;
+        emit CosignerRemoved(cosigner_);
+    }
+    
+    // ---------- Time Parameters ----------
+    
+    function _updateCommitCancellableTime(uint256 commitCancellableTime_) internal {
+        if (commitCancellableTime_ < MIN_COMMIT_CANCELLABLE_TIME) {
+            revert Errors.InvalidCommitCancellableTime();
+        }
+        uint256 oldCommitCancellableTime = commitCancellableTime;
+        commitCancellableTime = commitCancellableTime_;
+        emit CommitCancellableTimeUpdated(oldCommitCancellableTime, commitCancellableTime_);
+    }
+    
+    function _updateNftFulfillmentExpiryTime(uint256 nftFulfillmentExpiryTime_) internal {
+        if (nftFulfillmentExpiryTime_ < MIN_NFT_FULFILLMENT_EXPIRY_TIME) {
+            revert Errors.InvalidNftFulfillmentExpiryTime();
+        }
+        uint256 oldNftFulfillmentExpiryTime = nftFulfillmentExpiryTime;
+        nftFulfillmentExpiryTime = nftFulfillmentExpiryTime_;
+        emit NftFulfillmentExpiryTimeUpdated(oldNftFulfillmentExpiryTime, nftFulfillmentExpiryTime_);
+    }
+    
+    // ---------- Reward Limits ----------
+    
+    function _updateMinReward(uint256 minReward_) internal {
+        if (minReward_ == 0) revert Errors.InvalidReward();
+        if (minReward_ > maxReward) revert Errors.InvalidReward();
+        uint256 oldMinReward = minReward;
+        minReward = minReward_;
+        emit MinRewardUpdated(oldMinReward, minReward_);
+    }
+    
+    function _updateMaxReward(uint256 maxReward_) internal {
+        if (maxReward_ == 0) revert Errors.InvalidReward();
+        if (maxReward_ < minReward) revert Errors.InvalidReward();
+        uint256 oldMaxReward = maxReward;
+        maxReward = maxReward_;
+        emit MaxRewardUpdated(oldMaxReward, maxReward_);
+    }
+    
+    // ---------- Pack Price Limits ----------
+    
+    function _updateMinPackPrice(uint256 minPackPrice_) internal {
+        if (minPackPrice_ == 0) revert Errors.InvalidPackPrice();
+        if (minPackPrice_ > maxPackPrice) revert Errors.InvalidPackPrice();
+        uint256 oldMinPackPrice = minPackPrice;
+        minPackPrice = minPackPrice_;
+        emit MinPackPriceUpdated(oldMinPackPrice, minPackPrice_);
+    }
+    
+    function _updateMaxPackPrice(uint256 maxPackPrice_) internal {
+        if (maxPackPrice_ == 0) revert Errors.InvalidPackPrice();
+        if (maxPackPrice_ < minPackPrice) revert Errors.InvalidPackPrice();
+        uint256 oldMaxPackPrice = maxPackPrice;
+        maxPackPrice = maxPackPrice_;
+        emit MaxPackPriceUpdated(oldMaxPackPrice, maxPackPrice_);
+    }
+    
+    // ---------- Multipliers ----------
+    
+    function _updateMinPackRewardMultiplier(uint256 minPackRewardMultiplier_) internal {
+        if (minPackRewardMultiplier_ == 0) revert Errors.InvalidPackRewardMultiplier();
+        if (minPackRewardMultiplier_ > maxPackRewardMultiplier) revert Errors.InvalidPackRewardMultiplier();
+        uint256 oldMinPackRewardMultiplier = minPackRewardMultiplier;
+        minPackRewardMultiplier = minPackRewardMultiplier_;
+        emit MinPackRewardMultiplierUpdated(oldMinPackRewardMultiplier, minPackRewardMultiplier_);
+    }
+    
+    function _updateMaxPackRewardMultiplier(uint256 maxPackRewardMultiplier_) internal {
+        if (maxPackRewardMultiplier_ == 0) revert Errors.InvalidPackRewardMultiplier();
+        if (maxPackRewardMultiplier_ < minPackRewardMultiplier) revert Errors.InvalidPackRewardMultiplier();
+        uint256 oldMaxPackRewardMultiplier = maxPackRewardMultiplier;
+        maxPackRewardMultiplier = maxPackRewardMultiplier_;
+        emit MaxPackRewardMultiplierUpdated(oldMaxPackRewardMultiplier, maxPackRewardMultiplier_);
+    }
+    
+    // ---------- Fees ----------
+    
+    function _setProtocolFee(uint256 protocolFee_) internal {
+        if (protocolFee_ > BASE_POINTS) revert Errors.InvalidProtocolFee();
+        uint256 oldProtocolFee = protocolFee;
+        protocolFee = protocolFee_;
+        emit ProtocolFeeUpdated(oldProtocolFee, protocolFee_);
+    }
+    
+    function _setFlatFee(uint256 flatFee_) internal {
+        uint256 oldFlatFee = flatFee;
+        flatFee = flatFee_;
+        emit FlatFeeUpdated(oldFlatFee, flatFee_);
+    }
+    
+    // ---------- Funds Receiver ----------
+    
+    function _setFundsReceiver(address fundsReceiver_) internal virtual {
+        if (fundsReceiver_ == address(0)) revert Errors.FundsReceiverAddressZero();
+        if (_checkHasRole(FUNDS_RECEIVER_MANAGER_ROLE, fundsReceiver_)) {
+            revert Errors.InvalidFundsReceiverManager();
+        }
+        address oldFundsReceiver = fundsReceiver;
+        fundsReceiver = payable(fundsReceiver_);
+        emit FundsReceiverUpdated(oldFundsReceiver, fundsReceiver_);
+    }
+    
+    function _transferFundsReceiverManager(address newFundsReceiverManager_) internal virtual;
+    
+    function _checkHasRole(bytes32 role, address account) internal view virtual returns (bool);
+}
